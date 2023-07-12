3c9fe04c9b9e69a4a7841a3253f2eae68674196d
nicovillamonte <nicovillamonte@gmail.com>
2023-07-12 14:33:49 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, Signal, effect } from '@angular/core';
+import { Conversion } from 'src/DTO/conversion';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'eg-conversor-signals-angular';
+  millasInput: string = '';
+
+  // Parametros de conversion
+  millas = signal(0);
+  kilometros = computed(() => this.millas() * 1.60934);
+
+  // Parametros de referencia
+  isDecimal = computed(() => Number(this.millas().toFixed(2)) % 1 !== 0);
+
+  // Lista de conversiones guardadas
+  listaConversiones = signal<Conversion[]>([]);
+
+  constructor() {
+    effect(() => {
+      // Veremos como esto se llama solamente cuando cambia el valor de isDecimal
+      console.log(this.isDecimal() ? 'Es Decimal' : 'Es Entero');
+    });
+
+    effect(() => {
+      // Este effect se va a ejecutar tanto desde los cambios de listaConversiones en este componente como en el componente hijo
+      console.log(
+        'Se hizo un cambio en lista conversiones',
+        this.listaConversiones()
+      );
+    });
+  }
+
+  set(millas: number) {
+    if (!this.validateInput()) throw new Error('Valor no valido');
+    this.millas.set(millas);
+  }
+
+  incrementar(pasos: number = 1) {
+    this.millas.update((millas) => millas + pasos);
+  }
+
+  decrementar(pasos: number = 1) {
+    this.millas.update((millas) => millas - pasos);
+  }
+
+  guardarConversion(millas: number, kilometros: number) {
+    this.listaConversiones.mutate((lista) =>
+      lista.push({ millas, kilometros })
+    );
+  }
+
+  // --- Validaciones
+  validateInput(): boolean {
+    const value = this.millasInput.replace(',', '.');
+    return value == '' || !isNaN(Number(value));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { HistorialComponent } from './historial/historial.component';
+
+registerLocaleData(localeEs);
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, HistorialComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' },
+    {
+      provide: 'NUMBER_FORMATS',
+      useValue: { decimal: ',', thousands: '.' },
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
