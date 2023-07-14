3142b6284ab79e4f4edb3795b1500fca9efcfc5c
Fernando Dodino <fernando.dodino@gmail.com>
2023-07-13 22:27:06 -0300

    Fix CI - add karma config

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/eg-apuestas-angular'),
+      dir: require('path').join(__dirname, './coverage/eg-conversor-signals-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
